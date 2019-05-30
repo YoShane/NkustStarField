9613aeb7e974faf5b12ffc1cb9c75a1edc4f142e
YoShane <YoShane@users.noreply.github.com>
2019-05-30 18:39:59 +0800

    Initial commit

---
+++ EquipmentManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EquipmentManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
